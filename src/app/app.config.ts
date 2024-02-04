bf40cb38810eeaaac66045fc0c6fd9b3763e7663
Dave Szczesny <dave.szczesny@genesys.com>
2024-02-04 20:36:48 +0000

    Added firebase to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"pbcastlerea","appId":"1:936904557994:web:54686d74cf00de7c3cd7d3","storageBucket":"pbcastlerea.appspot.com","apiKey":"AIzaSyApQ5e5QvfxJP35SBzf_aro5xA_WiYWFUs","authDomain":"pbcastlerea.firebaseapp.com","messagingSenderId":"936904557994"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
