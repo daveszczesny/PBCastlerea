9a89f3190399089c79da0306649c23445993e048
Dave Szczesny <dave.szczesny@genesys.com>
2024-03-09 21:50:39 +0000

    Added a page not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ServicesComponent } from './services/services.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ServiceDetailComponent } from './service-detail/service-detail.component';
 
 
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'services', component: ServicesComponent},
-    {path: 'services/:id', component: ServiceDetailComponent},
     {path: '**', component:PageNotFoundComponent},
 ];
